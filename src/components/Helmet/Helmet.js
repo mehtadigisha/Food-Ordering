5895c47bf124e2a9bbaf3ce2d2f8ac4b09de78f2
mehtadigisha <123732235+mehtadigisha@users.noreply.github.com>
2024-06-18 16:00:50 +0530

    first

--- src/components/Helmet/Helmet.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Helmet = (props) => {
-  document.title = "Takeout Express - " + props.title;
-  return <div className="w-100">{props.children}</div>;
-};
-
-export default Helmet;
