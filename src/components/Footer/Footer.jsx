3763ff71bce9f447fb8ce321deb6333ee473eefd
mehtadigisha <123732235+mehtadigisha@users.noreply.github.com>
2024-06-18 16:17:27 +0530

    first

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
+
+import "../../styles/footer.css";
+
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className=" footer__logo text-start">
+              <img src={logo} alt="logo" />
+              <h5>TakeOut Express</h5>
+              <p>
+              Solution for easy and flexible getting meals for the household.
+              You can trust us anywhere through this platform
+              </p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Delivery Time</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Monday - Sunday</span>
+                <p>10:00am - 11:00pm</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Contact</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Phone: 9876512340</span>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Email: takeout@gmail.com</span>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Newsletter</h5>
+            <p>Subscribe our newsletter</p>
+            <div className="newsletter">
+              <input type="email" placeholder="Enter your email" />
+              <span>
+                <i class="ri-send-plane-line"></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className="mt-5">
+          <Col lg="6" md="6">
+            <p className="copyright__text">
+              Copyright - 2023,All Rights
+              Reserved.
+            </p>
+          </Col>
+          <Col lg="6" md="6">
+            <div className="social__links d-flex align-items-center gap-4 justify-content-end">
+              <p className="m-0">Follow: </p>
+              <span>
+                {" "}
+                <Link to="#">
+                  <i class="ri-facebook-line"></i>
+                </Link>{" "}
+              </span>
+
+              <span>
+                <Link to="#">
+                  <i class="ri-github-line"></i>
+                </Link>
+              </span>
+
+              <span>
+                {" "}
+                <Link to="#">
+                  <i class="ri-youtube-line"></i>
+                </Link>{" "}
+              </span>
+
+              <span>
+                {" "}
+                <Link to=" #">
+                  <i class="ri-linkedin-line"></i>
+                </Link>{" "}
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/UI/slider/TestimonialSlider.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Slider from "react-slick";
+
+import ava01 from "../../../assets/images/ava-1.jpg";
+import ava02 from "../../../assets/images/ava-2.jpg";
+import ava03 from "../../../assets/images/ava-3.jpg";
+
+import "../../../styles/slider.css";
+
+const TestimonialSlider = () => {
+  const settings = {
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    speed: 1000,
+    autoplaySpeed: 3000,
+    swipeToSlide: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <p className="review__text">
+        With a TasteOut Express meal, you can serve delectable, 
+        home-cooked meals to your family and friends in any location, 
+        anytime. anyday!
+        </p>
+        <div className=" slider__content d-flex align-items-center gap-3 ">
+          <img src={ava01} alt="avatar" className=" rounded" />
+          <h6>Jhon Doe</h6>
+        </div>
+      </div>
+      <div>
+        <p className="review__text">
+        Be it a small gathering of friends or a large event with hundreds of people, 
+        you'll always be prepared. whether you're at home or on the go.
+        </p>
+        <div className="slider__content d-flex align-items-center gap-3 ">
+          <img src={ava02} alt="avatar" className=" rounded" />
+          <h6>Mitchell Marsh</h6>
+        </div>
+      </div>
+      <div>
+        <p className="review__text">
+        No need to worry about any of this anymore with your app. 
+        TasteOut Express are made with the highest quality ingredients for you and your family.
+        </p>
+        <div className="slider__content d-flex align-items-center gap-3 ">
+          <img src={ava03} alt="avatar" className=" rounded" />
+          <h6>Steven Crock</h6>
+        </div>
+      </div>
+    </Slider>
+  );
+};
+
+export default TestimonialSlider;
